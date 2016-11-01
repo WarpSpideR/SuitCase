301456c1ca27c2475164534c09d77952d9a4b908
Maurice Davidson <maurice.davidson@youmanage.co.uk>
2016-11-01 15:08:10 +0000

    Create Benchmark Project

---
+++ SuitCase.Benchmarks/GenericCasingConverterBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuitCase.Benchmarks
+{
+    public class GenericCasingConverterBenchmarks
+    {
+
+        public GenericCasingConverter Converter { get; set; }
+
+        public GenericCasingConverterBenchmarks()
+        {
+            Converter = new GenericCasingConverter(new CasingSyntax()
+            {
+                TerminationType = TermTermination.Uppercase,
+                IncludeTerminator = true
+            });
+        }
+
+        [Benchmark]
+        public CasingContext FromCase()
+        {
+            return Converter.FromCase("SomeSimpleTestData");
+        }
+
+    }
+}
---
+++ SuitCase.Benchmarks/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuitCase.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var switcher = new BenchmarkSwitcher(new[] {
+                typeof(GenericCasingConverterBenchmarks)
+            });
+            switcher.Run(args);
+        }
+    }
+}
